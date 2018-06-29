4183df04a71a645e725c1ee766f450bef497b682
limonbiplob0155 <Limon0155$>
2018-06-29 12:52:09 +0600

    My laravel projrct

---
+++ footer.php
@@ -0,0 +1,16 @@
+			   </div>
+			    <div class="container-fluid">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                     <div class="copy">
+                        <h3>Copyright © 2018 Learn . All Rights Reserved  | Design by <a href="">LR.com</a> </h3>
+                  </div>
+                    </div>
+                  </div>
+	          </div>
+   
+       <script src="js/jquery-3.2.1.min%20(1).js"></script>
+       <script src="js/bootstrap.min.js"></script>
+       <script src="js/cycle2.js"></script>
+
+</body>
+</html>
